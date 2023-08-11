81b7badc1199255ddbdd1200e515bc2b8afd2fa0
mariquegonn-dev <ocatodev@gmail.com>
2023-08-11 18:25:10 -0300

    config: env example added

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,7 +1,8 @@
+require("custom-env").env("staging");
+
 const mongoose = require("mongoose");
 
-const dbConfig =
-  "mongodb+srv://mariquegonn:1212@cluster0.qvhmh0o.mongodb.net/anotacoes?retryWrites=true&w=majority";
+const dbConfig = process.env.DB_HOST;
 
 const connection = mongoose.connect(dbConfig, {
   useNewUrlParser: true,
